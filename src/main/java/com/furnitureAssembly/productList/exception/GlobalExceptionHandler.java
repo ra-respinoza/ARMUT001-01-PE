8385973071c6cf415d1a2df7c88bea1906357f39
Roger Espinoza Jaimes <roger0.espinoza1@gmail.com>
2026-02-22 11:14:46 -0500

    cambiando usuario de git

--- src/main/java/com/furnitureAssembly/productList/exception/GlobalExceptionHandler.java
+++ src/main/java/com/furnitureAssembly/productList/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(ex.getMessage());
